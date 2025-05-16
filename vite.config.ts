82b7332369932aa463410093dcd8b114f3190d48
Victorien <victoto56@gmail.com>
2025-05-16 12:14:52 +0200

    Add action

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  // Utilisez cette valeur pour GitHub Pages ou changez-la en fonction de votre dépôt
+  base: '/atelier-culs-nus/',
 })
